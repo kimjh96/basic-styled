fee5c396527c6c3d4d0f46c8d89a97faafa3be4b
김종혁 <kimjhs@kakao.com>
2024-09-01 14:09:10 +0900

    Initialized project

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import pluginJs from '@eslint/js';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginReact from 'eslint-plugin-react';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginPrettierRecommended,
+  {
+    plugins: {
+      'simple-import-sort': pluginSimpleImportSort
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'react',
+              importNames: ['default']
+            }
+          ]
+        }
+      ],
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+];
